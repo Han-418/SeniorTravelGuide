a53d04ff55c97770bb014ca687f13f4da1ab5000
user <gekd826@gmail.com>
2025-03-18 10:24:36 +0900

    필요없는 페이지 삭제

--- app/src/main/java/com/intel/NLPproject/RecommendTransportationScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.intel.NLPproject
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-
-@Composable
-fun RecommendTransportationScreen(navController: NavHostController) {
-
-}

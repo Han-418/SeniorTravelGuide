43538d93ea7bd8ace1b29311b90a7980bee4da99
user <gekd826@gmail.com>
2025-03-19 12:39:51 +0900

    CloudApiService(API) 인터페이스 수정 submitQuestion() -> POST taskStatus() -> GET

--- app/src/main/java/com/intel/NLPproject/CloudApiService.kt
+++ app/src/main/java/com/intel/NLPproject/CloudApiService.kt
@@ -2,7 +2,9 @@ package com.intel.NLPproject.api
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 // 서버로 전송할 데이터 모델
 data class QuestionData(
@@ -29,9 +31,16 @@ data class Recommendation(
 data class RecommendationResponse(
     val recommendations: List<Recommendation>
 )
+// 비동기 서버 응답 모댈
+data class TaskResponse(val task_id: String?)
+data class TaskStatusResponse(val state: String, val recommendations: List<Recommendation>?)
+
 
 // Google Cloud 서버에 요청할 API 인터페이스
 interface CloudApiService {
     @POST("submitQuestion")
-    suspend fun submitQuestion(@Body data: QuestionData): Response<RecommendationResponse>
+    suspend fun submitQuestion(@Body questionData: QuestionData): Response<TaskResponse>
+
+    @GET("taskStatus/{task_id}")
+    suspend fun getTaskStatus(@Path("task_id") taskId: String): Response<TaskStatusResponse>
 }

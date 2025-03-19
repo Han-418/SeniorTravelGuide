e245184296ff57acdc063beedf0302454886ffbe
user <gekd826@gmail.com>
2025-03-19 11:25:22 +0900

    BASE_URL: 다시 https -> http로 변경

--- app/src/main/java/com/intel/NLPproject/RetrofitClient.kt
+++ app/src/main/java/com/intel/NLPproject/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://34.64.58.211:5000/"
+    private const val BASE_URL = "http://34.64.58.211:5000/"
 
     val cloudApiService: CloudApiService by lazy {
         Retrofit.Builder()
